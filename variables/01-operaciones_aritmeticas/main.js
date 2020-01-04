a98e6e73aa00bec3b276120e3efc0df51c024a32
Johan Castillo <jcjohan2707@gmail.com>
2020-01-03 22:34:12 -0400

    Operaciones aritmeticas

---
+++ variables/01-operaciones_aritmeticas/main.js
@@ -0,0 +1,24 @@
+//Crear dos variables de tipo de dato númerico
+//sumar, restar, multiplicar y dividir su valor e imprimirlo.
+
+//Variables.
+var num_uno = 25;
+var num_dos = 25;
+
+//Sumar variables.
+var suma = num_uno + num_dos;
+
+//Restar variables.
+var resta = num_uno - num_dos;
+
+//Multiplicar variables.
+var multiplicacion = num_uno * num_dos;
+
+//Dividir variables.
+var division = num_uno / num_dos;
+
+//Imprimir el resultado de cada operacion.
+console.log("El resultado de la suma es de " + suma);
+console.log("El resultado de la resta es de " + resta);
+console.log("El resultado de la multiplicación es de " + multiplicacion);
+console.log("El resultado de la division es de " + division);
